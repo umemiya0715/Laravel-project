0e3004130eeced34bba3595a94d9d7ab711f959f
umemiya0715 <whiteblue116@icloud.com>
2023-03-29 10:56:35 +0900

    setup_react_and_typescript

---
+++ resources/ts/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+const container = document.getElementById('app');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+
+root.render(
+  // <React.StrictMode>
+    <div className="text-red">
+        <App />
+    </div>
+// </React.StrictMode>,
+);
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/hoge', function (Request $request) {
+    return response()->json(
+        [
+            'hoge' => 'Hello from Laravel'
+        ]
+    );
+});
